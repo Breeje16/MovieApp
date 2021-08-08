3fe375b7fc563d569e68b45fe1c4aed35fb7ed29
Breeje Modi <breeje2001@gmail.com>
2021-08-08 20:11:27 +0530

    Data Source Added

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "963bd5abf7a6f3ee550f8e9f735a5b92";
+  static const String BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500/";
+}
