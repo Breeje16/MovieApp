e3e602f3454d9ebc1c352345eea9016236de2841
Breeje Modi <breeje2001@gmail.com>
2021-08-09 14:07:47 +0530

    Data Injection Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movieapp/data/core/api_client.dart';
-import 'package:movieapp/data/data_sources/movie_remote_data_source.dart';
+import 'package:pedantic/pedantic.dart';
 
-void main() {
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  dataSource.getTrending();
-  dataSource.getPopular();
-  dataSource.getPlayingNow();
-  dataSource.getComingSoon();
+import 'di/get_it.dart' as getIt;
+import 'domain/entities/app_error.dart';
+import 'domain/entities/movie_entity.dart';
+import 'domain/entities/no_params.dart';
+import 'domain/usecases/get_trending.dart';
+
+Future<void> main() async {
+  unawaited(getIt.init());
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
+  final Either<AppError, List<MovieEntity>> eitherResponse =
+      await getTrending(NoParams());
+  eitherResponse.fold(
+    (l) {
+      print('error');
+      print(l);
+    },
+    (r) {
+      print('list of movies');
+      print(r);
+    },
+  );
   runApp(MyApp());
 }
 
@@ -19,62 +32,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
-        
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    
- 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
+      home: Container(),
     );
   }
 }
