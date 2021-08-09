acb67a4907aacf3411470d569639449382110d8b
Breeje Modi <breeje2001@gmail.com>
2021-08-09 17:27:28 +0530

    HomeScreen Carousel Added

--- lib/data/core/api_constants.dart
+++ lib/data/core/api_constants.dart
@@ -3,5 +3,5 @@ class ApiConstants {
 
   static const String BASE_URL = "https://api.themoviedb.org/3/";
   static const String API_KEY = "963bd5abf7a6f3ee550f8e9f735a5b92";
-  static const String BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500/";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500/";
 }
