3753b89d56d47030a8fbad9d3d282143822773a4
Breeje Modi <breeje2001@gmail.com>
2021-08-09 19:42:47 +0530

    Drawer and Tabs Added

--- lib/presentation/journeys/home/home_screen.dart
+++ lib/presentation/journeys/home/home_screen.dart
@@ -3,8 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movieapp/di/get_it.dart';
 import 'package:movieapp/presentation/blocs/movie_backdrop/movie_backdrop_bloc.dart';
 import 'package:movieapp/presentation/blocs/movie_carousel/movie_carousel_bloc.dart';
+import 'package:movieapp/presentation/blocs/movie_tabbed/movie_tabbed_bloc.dart';
+import 'package:movieapp/presentation/journeys/drawer/navigation_drawer.dart';
 
 import 'movie_carousel/movie_carousel_widget.dart';
+import 'movie_tabbed/movie_tabbed_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -14,12 +17,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   MovieCarouselBloc movieCarouselBloc;
   MovieBackdropBloc movieBackdropBloc;
+  MovieTabbedBloc movieTabbedBloc;
 
   @override
   void initState() {
     super.initState();
     movieCarouselBloc = getItInstance<MovieCarouselBloc>();
     movieBackdropBloc = movieCarouselBloc.movieBackdropBloc;
+    movieTabbedBloc = getItInstance<MovieTabbedBloc>();
     movieCarouselBloc.add(CarouselLoadEvent());
   }
 
@@ -28,6 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
     movieCarouselBloc?.close();
     movieBackdropBloc?.close();
+    movieTabbedBloc?.close();
   }
 
   @override
@@ -39,9 +45,13 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         BlocProvider(
           create: (context) => movieBackdropBloc,
-        )
+        ),
+        BlocProvider(
+          create: (context) => movieTabbedBloc,
+        ),
       ],
       child: Scaffold(
+        drawer: const NavigationDrawer(),
         body: BlocBuilder<MovieCarouselBloc, MovieCarouselState>(
           bloc: movieCarouselBloc,
           builder: (context, state) {
@@ -60,7 +70,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   FractionallySizedBox(
                     alignment: Alignment.bottomCenter,
                     heightFactor: 0.4,
-                    child: Placeholder(color: Colors.white),
+                    child: MovieTabbedWidget(),
                   ),
                 ],
               );
